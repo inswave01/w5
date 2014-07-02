edb412e2dc94e787f23af61c275cad4a685fd47c
maninzoo <maninzoo@inswave.com>
2014-07-02 13:10:36 +0900

    modify protocol

--- dist/require/requirejs.main.js
+++ dist/require/requirejs.main.js
@@ -3,7 +3,7 @@ requirejs.config({
     'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
     'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
     'Backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
-    'w5': 'https://w5.io/dist/js/w5.min.0.6.0'
+    'w5': 'http://w5.io/dist/js/w5.min.0.6.0'
   },
 
   shim: {
