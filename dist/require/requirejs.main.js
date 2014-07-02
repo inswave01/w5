81e0d20410c788d8cee018055886fe58bbce0b6e
maninzoo <maninzoo@inswave.com>
2014-07-02 13:46:35 +0900

    Update requirejs.main.js

--- dist/require/requirejs.main.js
+++ dist/require/requirejs.main.js
@@ -1,9 +1,9 @@
 requirejs.config({
   paths: {
-    'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-    'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
-    'Backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
-    'w5': 'http://w5.io/dist/js/w5.min.0.6.0'
+    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+    'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+    'Backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+    'w5': '//w5.io/dist/js/w5.min.0.6.0'
   },
 
   shim: {
