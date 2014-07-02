609ece3e59bf9415bb107fe8baec94bca15d607d
maninzoo <maninzoo@inswave.com>
2014-07-02 12:55:47 +0900

    remove extension

--- dist/require/requirejs.main.js
+++ dist/require/requirejs.main.js
@@ -1,13 +1,12 @@
 requirejs.config({
   paths: {
-    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
-    'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
-    'Backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js',
-    'w5': '//w5.io/dist/js/w5.min.0.6.0.js'
+    'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+    'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+    'Backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+    'w5': 'https://w5.io/dist/js/w5.min.0.6.0'
   },
 
   shim: {
-    'Backbone': ['jquery', 'underscore'],
-    'w5': ['Backbone']
+    'w5': ['jquery', 'underscore', 'Backbone']
   }
 });
