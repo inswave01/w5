2d4804e702601a5df7c44e2864a513fb46a1780d
maninzoo <maninzoo@inswave.com>
2014-07-02 11:23:53 +0900

    remove protocol

--- dist/require/requirejs.main.js
+++ dist/require/requirejs.main.js
@@ -1,9 +1,9 @@
 requirejs.config({
   paths: {
-    'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
-    'underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
-    'Backbone': 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js',
-    'w5': 'http://w5.io/dist/js/w5.min.0.6.0.js'
+    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
+    'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js',
+    'Backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js',
+    'w5': '//w5.io/dist/js/w5.min.0.6.0.js'
   },
 
   shim: {
