b37fbd89f43ea2fdbd28d42f70822a782474108f
maninzoo <maninzoo@inswave.com>
2014-07-22 18:08:04 +0900

    reModify #94 for fetch case (cherry picked from commit f201915)

--- src/grid.js
+++ src/grid.js
@@ -793,8 +793,7 @@ var GridProto = {
     if ( this.$wrapper_div ) {
       this.viewModel.setOption("scrollLeft", 0, {silent:true});
       this.viewModel.setOption("scrollTop", 0, {silent:true});
-      this.drawHeader();
-      this.drawTbody();
+      this.createTbody();
       this.setResize();
     }
   },
